b1516f0f02797d0479928ed9f0c843c3969dd7b4
AaronPrado <aaronpradodarriba@gmail.com>
2024-10-30 18:22:21 +0100

    script generador dataset

---
+++ generador_dataset.py
@@ -0,0 +1,23 @@
+from faker import Faker
+import pandas as pd
+
+seed_value = 42
+fake = Faker('es_ES')  
+fake.seed_instance(seed_value)
+num_records = 100  # Número de filas que queremos generar
+
+data = {
+    "Nombre": [fake.name() for _ in range(num_records)],
+    "Dirección": [fake.address() for _ in range(num_records)],
+    "Correo": [fake.email() for _ in range(num_records)],
+    "Teléfono": [fake.phone_number() for _ in range(num_records)],
+    "Fecha de nacimiento": [fake.date_of_birth(minimum_age=18, maximum_age=90) for _ in range(num_records)],
+    "Empresa": [fake.company() for _ in range(num_records)],
+    "Número de tarjeta": [fake.credit_card_number() for _ in range(num_records)],
+    "Fecha de vencimiento": [fake.credit_card_expire() for _ in range(num_records)],
+    "Código postal": [fake.postcode() for _ in range(num_records)],
+}
+
+df = pd.DataFrame(data)
+
+df.to_csv('dataset_falso.csv', index=False, encoding='utf-8')
